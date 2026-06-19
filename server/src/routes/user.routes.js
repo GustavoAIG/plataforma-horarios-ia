26f941653fb5bea770e2baddaac334c572c4b136
Percy Chabria <Percyalonzo@gmail.com>
2026-06-19 01:58:34 -0500

    feat: registrar ruta PATCH /profile para actualizar datos del ciclo

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { completeOnboarding } from '../controllers/user.controller.js'
+import { completeOnboarding, updateProfile } from '../controllers/user.controller.js'
 import { protect } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
 router.patch('/complete-onboarding', protect, completeOnboarding)
+router.patch('/profile', protect, updateProfile)
 
 export default router
